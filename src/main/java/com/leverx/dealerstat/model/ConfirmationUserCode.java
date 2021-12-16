4d96e9d5406caae47336d6a1a0e0d1adbb88b3c4
Siarhey-Skuratovich <siarheyskuratovich@gmail.com>
2021-12-16 20:02:39 +0300

    refactors

--- src/main/java/com/leverx/dealerstat/model/ConfirmationUserCode.java
+++ src/main/java/com/leverx/dealerstat/model/ConfirmationUserCode.java
@@ -1,6 +1,7 @@
 package com.leverx.dealerstat.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
@@ -8,18 +9,18 @@ import org.springframework.data.redis.core.index.Indexed;
 import java.util.UUID;
 
 @Getter
-@RedisHash(value = "Code", timeToLive = 86400)
+@NoArgsConstructor
+@RedisHash(value = "Code", timeToLive = ConfirmationUserCode.TIME_TO_LIVE_IN_SECONDS)
 public class ConfirmationUserCode {
   public static final int MIN_CODE_VALUE = 1000000000;
   public static final int MAX_CODE_VALUE = Integer.MAX_VALUE;
+  public static final int TIME_TO_LIVE_IN_SECONDS = 86400;
 
   @Id
   private UUID userId;
 
   @Indexed
   private int code;
-
-  private ConfirmationUserCode() {}
 
   public static Builder newBuilder() {
     return new ConfirmationUserCode().new Builder();
