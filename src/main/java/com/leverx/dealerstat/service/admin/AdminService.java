de67692c53209bf08dd0be140b22ae72f0e6fd20
Siarhey-Skuratovich <siarheyskuratovich@gmail.com>
2021-12-25 12:23:45 +0300

    refactored AdminService using Environment

--- src/main/java/com/leverx/dealerstat/service/admin/AdminService.java
+++ src/main/java/com/leverx/dealerstat/service/admin/AdminService.java
@@ -1,34 +1,25 @@
 package com.leverx.dealerstat.service.***REMOVED***;
 
 import com.leverx.dealerstat.model.UserEntity;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Properties;
 
 @Service
 public class AdminService {
 
-  private final Properties securityProperties;
+  private final Environment environment;
 
-  public AdminService() throws IOException, URISyntaxException {
-    FileReader reader = new FileReader(getClass().getClassLoader().getResource("security.properties").toURI().getPath());
-
-    Properties securityProperties = new Properties();
-    securityProperties.load(reader);
-
-    this.securityProperties = securityProperties;
+  public AdminService(Environment environment)  {
+    this.environment = environment;
   }
 
   public String getAdminLogin() {
-    return securityProperties.getProperty("***REMOVED***.login");
+    return environment.getProperty("***REMOVED***.login");
   }
 
   public String getEncryptedAdminPassword() {
-    return new BCryptPasswordEncoder().encode(securityProperties.getProperty("***REMOVED***.password"));
+    return new BCryptPasswordEncoder().encode(environment.getProperty("***REMOVED***.password"));
   }
 
   public String getAdminRole() {
